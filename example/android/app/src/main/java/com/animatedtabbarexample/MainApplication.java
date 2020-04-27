3bf4bd15d2b8758a3bac6c1001d1fc73ae19899a
Mo Gorhom <gorhom@me.com>
2020-04-27 12:19:05 +0200

    chore: updated android icons

--- example/android/app/src/main/java/com/animatedtabbarexample/MainApplication.java
+++ example/android/app/src/main/java/com/animatedtabbarexample/MainApplication.java
@@ -1,4 +1,4 @@
-package com.AnimatedTabbarExample;
+package com.animatedtabbarexample;
 
 import android.app.Application;
 import android.content.Context;
@@ -9,7 +9,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
 
 
 public class MainApplication extends Application implements ReactApplication {
@@ -27,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for AnimatedTabbarExample:
           // packages.add(new MyReactNativePackage());
-          
-          
+
+
           return packages;
         }
 
