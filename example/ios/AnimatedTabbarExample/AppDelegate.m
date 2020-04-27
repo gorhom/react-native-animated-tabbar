ecc674e926ddd6e15057d1dba2f1db60b8c0d813
Mo Gorhom <gorhom@me.com>
2020-04-27 13:12:01 +0200

    feat: update library branding (#27)
    * chore: updated ios icons
    
    * chore: updated android icons

--- example/ios/AnimatedTabbarExample/AppDelegate.m
+++ example/ios/AnimatedTabbarExample/AppDelegate.m
@@ -20,7 +20,7 @@
                                                    moduleName:@"AnimatedTabbarExample"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.0f green:0.0f blue:0.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
