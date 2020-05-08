a20be75bdcb28c1785cd1a767f5dd5436b86a207
Mo Gorhom <gorhom@me.com>
2020-05-09 01:37:44 +0200

    chore: implement an optimize withTransition

---
+++ src/withTransition.ts
@@ -0,0 +1,78 @@
+import Animated from 'react-native-reanimated';
+import { TabBarAnimationConfigurableProps } from './types';
+
+const {
+  block,
+  cond,
+  onChange,
+  Value,
+  Clock,
+  set,
+  eq,
+  neq,
+  not,
+  or,
+  timing,
+  and,
+  startClock,
+  clockRunning,
+  stopClock,
+} = Animated;
+
+interface withTransitionProps
+  extends Required<TabBarAnimationConfigurableProps> {
+  index: number;
+  selectedIndex: Animated.Value<number>;
+}
+
+export const withTransition = ({
+  index,
+  selectedIndex,
+  duration,
+  easing,
+}: withTransitionProps) => {
+  //#region variables
+  const clock = new Clock();
+  const state = {
+    finished: new Value(0),
+    frameTime: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+  };
+  const config = {
+    toValue: new Value(0),
+    easing,
+    duration,
+  };
+  //#endregion
+
+  //#region conditions
+  const shouldAnimateIn = and(eq(selectedIndex, index), neq(state.position, 1));
+  const shouldAnimateOut = and(
+    neq(selectedIndex, index),
+    neq(state.position, 0)
+  );
+  const shouldAnimate = or(shouldAnimateIn, shouldAnimateOut);
+  //#endregion
+  const finishTiming = [
+    stopClock(clock),
+    set(state.finished, 0),
+    set(state.frameTime, 0),
+    set(state.time, 0),
+  ];
+  return block([
+    onChange(selectedIndex, finishTiming),
+    cond(shouldAnimate, [
+      cond(and(not(clockRunning(clock)), not(state.finished)), [
+        set(state.frameTime, 0),
+        set(state.time, 0),
+        set(state.finished, 0),
+        set(config.toValue, shouldAnimateIn),
+        startClock(clock),
+      ]),
+      timing(clock, state, config),
+      cond(state.finished, finishTiming),
+    ]),
+    state.position,
+  ]);
+};
