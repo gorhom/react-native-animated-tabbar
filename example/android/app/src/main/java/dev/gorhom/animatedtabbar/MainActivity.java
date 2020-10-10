6fed8eb175621ecd39a2589b1204eaa427e06a8f
Mo Gorhom <>
2020-10-11 00:05:04 +0200

    chore: updated android project

--- example/android/app/src/main/java/dev/gorhom/animatedtabbar/MainActivity.java
+++ example/android/app/src/main/java/dev/gorhom/animatedtabbar/MainActivity.java
@@ -1,4 +1,4 @@
-package com.animatedtabbarexample;
+package dev.gorhom.animatedtabbar;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- example/android/app/src/main/java/dev/gorhom/animatedtabbar/MainApplication.java
+++ example/android/app/src/main/java/dev/gorhom/animatedtabbar/MainApplication.java
@@ -1,9 +1,10 @@
-package com.animatedtabbarexample;
+package dev.gorhom.animatedtabbar;
 
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -26,8 +27,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for AnimatedTabbarExample:
           // packages.add(new MyReactNativePackage());
-
-
           return packages;
         }
 
@@ -46,23 +45,28 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this); // Remove this line if you don't want Flipper enabled
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
-   * Loads Flipper in React Native templates.
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
    *
    * @param context
+   * @param reactInstanceManager
    */
-  private static void initializeFlipper(Context context) {
+  private static void initializeFlipper(
+      Context context, ReactInstanceManager reactInstanceManager) {
     if (BuildConfig.DEBUG) {
       try {
         /*
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.facebook.flipper.ReactNativeFlipper");
-        aClass.getMethod("initializeFlipper", Context.class).invoke(null, context);
+        Class<?> aClass = Class.forName("com.test.ReactNativeFlipper");
+        aClass
+            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+            .invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
